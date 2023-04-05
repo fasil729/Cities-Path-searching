d86a4a79261f08603c24a150eba4e2b3f92bc62f
fasika <fasikafikadu38@gmail.com>
2023-04-05 15:51:17 +0300

    update the graph

--- graph implemntation/graph.py
+++ graph implemntation/graph.py
@@ -1,116 +1,79 @@
-class Node():
-   def __init__(self):
-#acyclic
-from collections import defaultdict
+class Node:
+    """
+    consructs city nodes with adjacency list of the nodes
+    """
+    def __init__(self, item):
+        self.item = item
+        self.adjacent_nodes = []
 
+    def add_adjacent_node(self, node, cost):
+        self.adjacent_nodes.append((node, cost))
+    def delete_adjacent_node(self, node):
+        self.adjacent_nodes = [tup for tup in self.adjacent_nodes if node not in tup]
 
-def has_path(graph, src, dst):
-  """
-  checks if dst is approchable starting from src in given acyclic graph
-  """
-  if dst in graph[src]:
-    return True
-  res = False
-  for neighbour in graph[src]:
-    res = res or has_path(graph, neighbour, dst)
-  return res
-
-# cyclic
-def has_path(graph, src, dst, visited):
-    """
-    checks if dst is approchable starting from src in given cyclic graph
-    """
-    if dst in graph[src]:
-        return True
-    visited.add(src)
-    res = False
-    for neighbour in graph[src]:
-        if neighbour not in visited:
-            res = res or has_path(graph, neighbour, dst, visited)
-    return res
-
-def bulidUndirectedGraph(edges):
-    """
-    builds undirected graph using the given edges
-    """
-    graph = defaultdict(list)
-    for edge in edges:
-        a, b = edge
-        graph[a].append(b)
-        graph[b].append(a)
-    return graph
-
-def connected_components_count(graph):
-    """
-    Count connected compenents in graph
-    """
-    visited = set()
-    count = 0
-    for node in graph:
-      if node in visited:
-        continue
-      traverse(graph, node, visited)
-      count += 1
-    return count
-  
- # needs to fix what to be returned 
-def traverse(graph, src, visited):
-  """traverse through the graph using dfs traverse"""
-  if src in visited:
-    return   #visited
-  visited.add(src)
-  for neighbour in graph[src]:
-    traverse(graph, neighbour, visited)
-  return      #visited    
-
-def largest_component(graph):
-    """
-    find largest compenent of the graph interms of
-    the number of nodes the compenent contains
+class Graph:
+    def __init__(self):
+        self.graph = set()
     
-    """
-    visited = set()
-    count = 0
-    for node in graph:
-      if node in visited:
-        continue
-      count = max(count, num_of_nodes(graph, node, visited))
-    return count
-  
- 
-def num_of_nodes(graph, src, visited):
-  """ calculates number of nodes in given graph compenent"""
-  if src in visited:
-    return 0
-  visited.add(src)
-  res = 1
-  for neighbour in graph[src]:
-    res += num_of_nodes(graph, neighbour, visited)
-  return res
+    def create_node(self, item):
+        """
+        create node
+        """
+        node = Node(item)
+        return node
+    
+    def insert_node(self, node):
+        """
+        inserts the node to the graph
+        """
+        self.graph.add(node)
+    
+    def delete_node(self, node):
+        """
+        delete the node from the graph
+        """
+        self.graph.remove(node)
+        del node
+    
+    def insert_edge(self, node_A, node_B):
+        """
+        inserts the edge to beteween node_A and node_B
+        """
+        node_A.add_adjacent_node(node_B)
+        node_B.add_adjacent_node(node_A)
+    
+    def delete_edge(self, node_A, node_B):
+        """
+        deletes the edge beteween node_A and node_B
+        """
+        node_A.delete_adjacent_node(node_B)
+        node_B.delete_adjacent_node(node_A)
 
-def shortest_path(edges, node_A, node_B):
-  """ 
-  calculates the shortest num of edges from node_A to node_B and return it if 
-  node_B exitst otherwise return -1
-  """
-  graph = bulidUndirectedGraph(edges)
-  visited = set()
-  start = [[node_A, 0]]
-  queue = deque(start)
-  print(graph)
-  while queue:
-    
-    curr, dist = queue.popleft()
-    print(queue, curr, visited, graph[curr])
-    if curr == node_B:
-      return dist
-    if curr in visited:
-      continue
-    
-    visited.add(curr)
-    for neighbour in graph[curr]:
-      queue.append([neighbour, dist + 1])
-  return -1
+    def search_item(self, item):
+        """
+        search for item in the graph and return it's node if it exists other wise None
+        """
+        for node in self.graph:
+            if node.item == item:
+                return node
+        return None
+
+    def has_path(self, src, dst, visited=set()):
+      """
+      checks if dst is approchable starting from src in given cyclic graph
+      """
+      if dst in src.adjacent_nodes:
+          return True
+      visited.add(src)
+      res = False
+      for neighbour, cost in src.adjacent_nodes:
+          if neighbour not in visited:
+              res = res or self.has_path(neighbour, dst, visited)
+      return res
+
+
+
+
 
 
 
